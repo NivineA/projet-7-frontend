5ac1a28714da621974c94dc708b6ce0956337332
NivineA <nivine.attoue@gmail.com>
2022-01-05 21:25:26 +0100

    changing local url

--- dashboard.py
+++ dashboard.py
@@ -15,7 +15,7 @@ import plotly.graph_objects as go
 from traitlets.traitlets import default
 
 
-URL_API = 'http://127.0.0.1:5000//'
+URL_API = 'https://projet-7-backend.herokuapp.com/'
 
 def main():
 
